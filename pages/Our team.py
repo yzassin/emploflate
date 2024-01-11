c79a90fb70dd16cae2c90791f2c4a927bd724efe
NZ <147848898+yzassin@users.noreply.github.com>
2024-01-11 19:19:14 +0800

    Create Our team.py

---
+++ pages/Our team.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import streamlit as st
+
+st.header('EMPLOFLATE')
+st.markdown("""
+- A web app provides a centralized platform for a comprehensive view of both unemployment and inflation rates, enabling stakeholders to understand the overall economic health in one glance.
+- Benficiary : Decision-makers, policymakers, and business leaders 
+- Domain : Economy and Government
+""")
+
+st.header('Future Directions')
+st.markdown("""
+- Integrate machine learning model directly into the dashboard to provide users with valuable insights.
+- Implement a real-time or scheduled data update feature so  users always have access to the latest economic data
+- Introduce the ability to compare inflation and unemployment which users could select different variables by state, time frames, or economic indicators for comparison.
+""")
+
+st.subheader('The team')
+st.code("""
+        - Vinod as Leader
+        - Nissa as Secretary
+        - Shakirah as Detective
+        - Hovinya as Oracle
+        - Atiqah as Maker 
+""")
+
+
+
+#st.write(['st', 'is <', 3]) # see *
